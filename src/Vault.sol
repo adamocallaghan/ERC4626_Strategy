f383157024c8ccf166d50c675af50b4db2c607e7
adamocallaghan <adam.ocallaghan@yahoo.ie>
2024-08-06 13:48:29 +0100

    OZ contracts addded & Ownable used

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.13;
 
 import {ERC4626} from "lib/solmate/src/tokens/ERC4626.sol";
 import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
-// import {IERC20} from "lib/forge-std/src/interfaces/IERC20.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Vault is ERC4626 {
+contract Vault is ERC4626, Ownable {
     // STORAGE VARS
     address strategy;
 
     constructor(ERC20 _asset, string memory _name, string memory _symbol, address _strategy)
         ERC4626(_asset, _name, _symbol)
+        Ownable(msg.sender)
     {
         strategy = _strategy;
     }
